1da1d9eb0367a854c5c22df967bf0cf254faade6
Fouad <fouadnasser1998@gmail.com>
2022-05-11 21:41:41 +0200

    create end game class

---
+++ Core/EndGame.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using Objects.Menu;
+
+namespace Snake.Core
+{
+    static class EndGame
+    {
+         public static bool NewGame{ get; set;}
+        private static void ShowMenu()
+        {
+            Menu m;
+            List<string> items;
+            items = new List<string>();
+            items.Add("Play Again");
+            items.Add("End Game");
+            
+
+            m = new Menu(items, ConsoleColor.DarkCyan, ConsoleColor.White,2,10);
+            m.PressEnter += m_PressEnter;
+            m.show();
+        }
+        private static void m_PressEnter(object sender,MenuEvenrArgs e)
+        {
+            switch (e.Current)
+            {
+                case 0:
+                    e.IsChoice = true;
+                    Console.Clear();
+                    NewGame = true;
+                    break;
+                case 1:
+                    e.IsChoice = true;
+                    System.Environment.Exit(-1);
+                    break;
+            }
+        }
+    }
+}
